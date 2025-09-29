d72416a87829cafb6bbc1dcb1c5715dab55e70b8
Mafaka77 <zuala4@gmail.com>
2025-09-29 14:50:32 +0530

    fcm

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ connectDB();
 dotenv.config();
 
 // Initialize Admin SDK
-const credPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+const credPath = process.env.GOOGLE_APPLICATION_CREDENTIALS || "./serviceAccount.json";
 const absCredPath = path.isAbsolute(credPath) ? credPath : path.join(process.cwd(), credPath);
 const serviceAccount = JSON.parse(fs.readFileSync(absCredPath, "utf8"));
 
