5ed000726f037c2d5a94d8225b71c9ba21df4ad6
Mafaka77 <zuala4@gmail.com>
2026-04-24 17:22:26 +0530

    sdasdasdasd

---
+++ dist/assets/evaluationParent-BVgYp9tY.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{o as r,c as t,b as c,i as n}from"./index-CPHcBP7B.js";const a={},s={class:"page-wrapper"};function _(i,p){const e=n("router-view");return r(),t("div",s,[c(e)])}const l=o(a,[["render",_]]);export{l as default};
